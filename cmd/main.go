162e87e84e4b33543919fd3b2f7b2974c36cb5a3
cyberdr0id <70664598+cyberdr0id@users.noreply.github.com>
2022-06-13 11:48:26 +0300

    API testing (#6)
    * Create config file
    
    * Implemented API tests
    
    * Update workflow

--- cmd/main.go
+++ cmd/main.go
@@ -6,17 +6,16 @@ import (
 
 	"github.com/cyberdr0id/bank-backend/api"
 	db "github.com/cyberdr0id/bank-backend/db/sqlc"
+	"github.com/cyberdr0id/bank-backend/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("unbale to load config: ", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database:", err)
 	}
@@ -24,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	if err := server.Start(serverAddress); err != nil {
+	if err := server.Start(config.ServerAddress); err != nil {
 		log.Fatal("cannot start server", err)
 	}
 }
